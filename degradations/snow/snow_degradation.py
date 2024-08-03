161057d5358ee170690c982f151df5db5c554f61
Harsh Lunia <38329268+harshlunia7@users.noreply.github.com>
2024-08-04 00:04:19 +0530

    snowDegradation works

--- degradations/snow/snow_degradation.py
+++ degradations/snow/snow_degradation.py
@@ -12,31 +12,26 @@ from ..base_degradation import Degradation
 class SnowDegradation(Degradation):
     def __init__(self, degree, image_type):
         super().__init__(degree, image_type)
-        self.number_of_streaks = [70, 90]
-        self.line_width = [1, 2]
+        self.package_dir = os.path.dirname(os.path.abspath(__file__))
+        self.snow_mask_directories = [os.path.join(self.package_dir, '..', '..', 'data', 'snow_masks', 'mid_masks'), 
+                                      os.path.join(self.package_dir, '..', '..', 'data', 'snow_masks', 'combined_masks')]
+    
+    def add_snow_streaks(self, real_image, snow_mask):
+        resized_snow_mask = cv2.resize(snow_mask, (real_image.shape[1], real_image.shape[0]))
+        
+        resized_snow_mask_3ch = cv2.merge([resized_snow_mask] * 3)
+        normalized_snow_mask = resized_snow_mask_3ch / 255.0
+        snowy_image = cv2.addWeighted(real_image, 1, resized_snow_mask_3ch, 0.5, 0)
+        return snowy_image
     
     def apply(self, image):
         if self.image_type == "full":
             print("Not Implemented for Full Image!")
             sys.exit(0)
         
-        w, h, n_channels = image.shape
-        isgray = n_channels == 1
+        snow_mask_filename = np.random.choice(os.listdir(self.snow_mask_directories[self.degree]))
+        snow_mask_path = os.path.join(self.snow_mask_directories[self.degree], snow_mask_filename)
+        snow_mask = cv2.imread(snow_mask_path, cv2.IMREAD_GRAYSCALE)
 
-        line_width = self.line_width[self.width]
-        n_rains = self.rng.integers(self.number_of_streaks[self.degree], self.number_of_streaks[self.degree] + 20)
-        slant = self.rng.integers(-60, 60)
-        fillcolor = 200 if isgray else (200, 200, 200)
-
-        max_length = min(w, h, 10)
-        for i in range(1, n_rains):
-            length = self.rng.integers(5, max_length)
-            x1 = self.rng.integers(0, w - length)
-            y1 = self.rng.integers(0, h - length)
-            x2 = x1 + length * math.sin(slant * math.pi / 180.)
-            y2 = y1 + length * math.cos(slant * math.pi / 180.)
-            x2 = int(x2)
-            y2 = int(y2)
-            cv2.line(image, (x1, y1), (x2, y2), fillcolor, line_width)
-
-        return image
+        snowy_image = self.add_snow_streaks(image, snow_mask)
+        return snowy_image
